39d420337148a98e0da03ec43bf683de8b0afe98
Arkadiy <alibullterrier@gmail.com>
2024-04-30 09:29:22 +0300

    refactor: remove redux from all project, rewrite store, remove combineReducers, add object. Add dispatch from Redux Toolkit. Fixed all tests, tests is done, without errors. Rewrite tasks reducer to RTK, fixed all bugs

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,22 +1,21 @@
 import { tasksReducer } from '../features/TodolistsList/tasks-reducer'
 import { todolistsReducer } from '../features/TodolistsList/todolists-reducer'
-import { combineReducers } from 'redux'
 import { ThunkAction, ThunkDispatch } from 'redux-thunk'
 import { appReducer } from './app-reducer'
 import { authReducer } from '../features/Login/auth-reducer'
 import { configureStore, UnknownAction } from '@reduxjs/toolkit'
 
-const rootReducer = combineReducers({
-  tasks: tasksReducer,
-  todolists: todolistsReducer,
-  app: appReducer,
-  auth: authReducer,
+//  const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+export const store = configureStore({
+  reducer: {
+    tasks: tasksReducer,
+    todolists: todolistsReducer,
+    app: appReducer,
+    auth: authReducer,
+  },
 })
 
-//  const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
-export const store = configureStore({ reducer: rootReducer })
-
-export type AppRootStateType = ReturnType<typeof rootReducer>
+export type AppRootStateType = ReturnType<typeof store.getState>
 
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
